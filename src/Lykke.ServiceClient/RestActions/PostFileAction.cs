e290a017b60777255cdccf8c985a7c3b3f8585f8
Albert Ibragimov <a@prosto-a.ru>
2018-06-06 16:29:36 +0300

    add PostMultipartFormAction

--- src/Lykke.ServiceClient/RestActions/PostFileAction.cs
+++ src/Lykke.ServiceClient/RestActions/PostFileAction.cs
@@ -1,35 +1,14 @@
-﻿using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-
-using Newtonsoft.Json;
+﻿using System.Collections.Generic;
 
 namespace Lykke.ServiceClient {
-    public class PostFileAction : RestActionBase {
-        public PostFileAction(string action, string name, FileModel file, object data = null) {
+    public class PostFileAction : PostMultipartFormAction
+    {
+        public PostFileAction(string action, string name, FileModel file) {
             Action = action;
-            Name = name;
-            File = file;
-            Data = data;
+            Data = new Dictionary<string, object> { { name, file } };
         }
 
         protected override string Action { get; }
-        protected override HttpMethod HttpMethod { get; } = HttpMethod.Post;
-
-        protected string Name { get; }
-        protected FileModel File { get; }
-        public object Data { get; }
-
-        protected override string LogDetails => $"{File.Name}";
-
-        protected override void FillRequest(HttpRequestMessage request) {
-            var fileContent = new ByteArrayContent(File.Content);
-            fileContent.Headers.ContentType = new MediaTypeHeaderValue(File.ContentType);
-
-            request.Content = new MultipartFormDataContent {
-                { fileContent, Name, File.Name },
-                { new StringContent(JsonConvert.SerializeObject(Data), Encoding.UTF8, "application/json"), "data" }
-            };
-        }
+        protected override IDictionary<string, object> Data { get; }
     }
 }
---
+++ src/Lykke.ServiceClient/RestActions/PostMultipartFormAction.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace Lykke.ServiceClient {
+    public abstract class PostMultipartFormAction : RestActionBase
+    {
+        protected abstract IDictionary<string, object> Data { get; }
+
+        protected override HttpMethod HttpMethod { get; } = HttpMethod.Post;
+
+        protected override void FillRequest(HttpRequestMessage request)
+        {
+            var content = new MultipartFormDataContent();
+
+            foreach (var item in Data)
+            {
+                switch (item.Value) {
+                    case null:
+                        break;
+
+                    case FileModel fileModel:
+                        var fileContent = new ByteArrayContent(fileModel.Content);
+                        fileContent.Headers.ContentType = new MediaTypeHeaderValue(fileModel.ContentType);
+                        content.Add(fileContent, item.Key, fileModel.Name);
+                        break;
+
+                    default:
+                        var jsonContent = new StringContent(JsonConvert.SerializeObject(Data), Encoding.UTF8, "application/json");
+                        content.Add(jsonContent, item.Key);
+                        break;
+                }
+            }
+
+            request.Content = content;
+        }
+    }
+}
